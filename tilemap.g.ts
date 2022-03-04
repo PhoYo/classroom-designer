7f70c4d17f6adb165c1571d46766b66b0ed841a5
Philip Barker <philipbarker1012@googlemail.com>
2022-03-04 10:19:22 +0000

    added blackboard

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -70,6 +70,18 @@ namespace myTiles {
     export const tile32 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile34 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile36 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile37 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile38 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile39 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile40 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile35 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -93,7 +105,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`20001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000070d0f0000000000000000000000000000000000000000000000000000000706181a0c0e0f0000000000000000000000000000000000000000000000070618190305080c0c0e0f000000000000000000000000000000000000000006181903020101010b080c0c0e16000000000000000000000000000000000000040302010101010101010b080c170000000000000000000000000000000000001001010101010101010101010b15000000000000000000000000000000000000110a0901010101010101011314120000000000000000000000000000000000000000110a0901010101131412000000000000000000000000000000000000000000000000110a091314120000000000000000000000000000000000000000000000000000000011120000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+            case "level1":return tiles.createTilemap(hex`20001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000070d0f0000000000000000000000000000000000000000000000000000000706181a0c1d0f0000000000000000000000000000000000000000000000070618190305081e1c200f000000000000000000000000000000000000000006181903020101010b1b1f0c0e16000000000000000000000000000000000000040302010101010101010b080c170000000000000000000000000000000000001001010101010101010101010b15000000000000000000000000000000000000110a0901010101010101011314120000000000000000000000000000000000000000110a0901010101131412000000000000000000000000000000000000000000000000110a091314120000000000000000000000000000000000000000000000000000000011120000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
 ................................
 ................................
 ................................
@@ -110,7 +122,7 @@ namespace myTiles {
 ................................
 ................................
 ................................
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile19,myTiles.tile20,myTiles.tile21,myTiles.tile22,myTiles.tile23,myTiles.tile13,myTiles.tile14,myTiles.tile18,myTiles.tile17,myTiles.tile16,myTiles.tile27,myTiles.tile28,myTiles.tile29,myTiles.tile31,myTiles.tile32,myTiles.tile34], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile19,myTiles.tile20,myTiles.tile21,myTiles.tile22,myTiles.tile23,myTiles.tile13,myTiles.tile14,myTiles.tile18,myTiles.tile17,myTiles.tile16,myTiles.tile27,myTiles.tile28,myTiles.tile29,myTiles.tile31,myTiles.tile32,myTiles.tile34,myTiles.tile36,myTiles.tile35,myTiles.tile37,myTiles.tile38,myTiles.tile39,myTiles.tile40], TileScale.Sixteen);
         }
         return null;
     })
@@ -186,6 +198,18 @@ namespace myTiles {
             case "tile32":return tile32;
             case "myTile32":
             case "tile34":return tile34;
+            case "myTile34":
+            case "tile36":return tile36;
+            case "myTile35":
+            case "tile37":return tile37;
+            case "myTile36":
+            case "tile38":return tile38;
+            case "myTile37":
+            case "tile39":return tile39;
+            case "myTile38":
+            case "tile40":return tile40;
+            case "myTile33":
+            case "tile35":return tile35;
         }
         return null;
     })
